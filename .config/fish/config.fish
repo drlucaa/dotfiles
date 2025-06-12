16e5672126b0bdfa9a726425100101ff3ca3a806
Luca Fondo <luca.fondo@dvbern.ch>
2025-06-12 15:32:58 +0200

    feat(fish): introducing new config for fish shell

---
+++ .config/fish/config.fish
@@ -0,0 +1,18 @@
+# Interactive context
+if status is-interactive
+    # Set Abbreviations
+    abbr lg 'lazygit'
+
+    # Set Editor Variable
+    set -gx EDITOR "zed --wait"
+    set -gx VISUAL "zed --wait"
+
+    # Initialize tools
+    starship init fish | source
+    atuin init fish | source
+    zoxide init --cmd cd fish | source
+    mise activate fish | source
+end
+
+# Add shims for non interactive contexts
+mise activate fish --shims | source
