6e8f497b7b6ffeb666b6c02f9e37fed2cecc6e69
Luca Fondo <luca.fondo@trai.ch>
2025-06-15 21:00:15 +0200

    feat(nvim): start new config from scratch

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -2,3 +2,4 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 vim.keymap.set("i", "jj", "<Esc>", { noremap = true })
+vim.keymap.set("n", "U", "<C-r>", { noremap = true })
